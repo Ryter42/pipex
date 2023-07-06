4e389eef67fdefb76df1764e4a0e0146b67980dd
elias <elias@moreau.in>
2023-07-06 18:44:43 +0200

    test

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 23:51:39 by elias             #+#    #+#             */
-/*   Updated: 2023/07/02 17:42:44 by elias            ###   ########.fr       */
+/*   Updated: 2023/07/06 18:44:10 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,15 +75,35 @@ char	**addslash(char **env)
 	return (path);
 }
 
+int	**create_tab_fd(int size)
+{
+	int	**fd;
+	int	i;
+
+	i = 0;
+	fd = malloc(sizeof(int **) * (size + 1));
+	if (!fd)
+		return (NULL);
+	while (i < size)
+	{
+		fd[i] = malloc(sizeof(int *) * 2);
+		i++;
+	}
+	fd[i] = NULL;
+	return (fd);
+}
+
 t_data	*init(int ac, char **av, char **env)
 {
 	t_data	*data;
 
 	data = malloc(sizeof(t_data));
+	data->fd = create_tab_fd(ac - 3);
 	data->path = addslash(env);
 	data->av = av;
 	data->env = env;
 	data->ac = ac;
+	data->index = 2;
 	return (data);
 }
 
@@ -103,25 +123,123 @@ int	findpath(char **path, char *cmd)
 	return (-1);
 }
 
+void	get_cmd(t_data *data)
+{
+	data->file = findpath(data->path, data->av[data->index]);
+	data->cmd = ft_strjoin(data->path[data->file], data->av[data->index]);
+	data->arg = ft_split(data->av[data->index], ' ');
+}
+
+void	firstcmd(t_data *data)
+{
+	// int fd;
+
+	data->file = findpath(data->path, data->av[data->index]);
+	data->cmd = ft_strjoin(data->path[data->file], data->av[data->index]);
+	data->arg = ft_split(data->av[data->index], ' ');
+	// fd = open(data->av[1], O_RDONLY);
+	close(data->fd[data->index - 2][0]);
+	// close(data->fd[0]);
+	// dup2(data->fd[1], STDOUT_FILENO);
+	// close(data->fd[1]);
+	// dup2(fd, STDIN_FILENO);
+	dup2(data->fd[data->index - 2][1], STDOUT_FILENO);
+	close(data->fd[data->index - 2][1]);
+	// dprintf(2, "arg = %s\n", data->arg[0]);
+	// dprintf(2, "arg = %s\n", data->arg[1]);
+	// dprintf(2, "data->cmd = %s\n", data->cmd);
+	write(2, data->arg[0], ft_strlen(data->arg[0]));
+	execve(data->cmd, data->arg, data->env);	
+}
+
+// void	midlecmd(t_data *data)
+// {
+// 	data->file = findpath(data->path, data->av[data->index]);
+// 	data->cmd = ft_strjoin(data->path[data->file], data->av[data->index]);
+// 	data->arg = ft_split(data->av[data->index], ' ');
+// 	close(data->fd[data->index - 3][1]);
+// 	close(data->fd[data->index - 2][0]);
+// 	dup2(data->fd[data->index - 1][0], STDIN_FILENO);
+// 	dup2(data->fd[data->index - 2][1], STDOUT_FILENO);
+// 	dprintf(2, "%s\n", data->arg[0]);
+// 	dprintf(2, "%s\n", data->arg[1]);
+// 	dprintf(2, "data->cmd = %s\n", data->cmd);
+// 	execve(data->cmd, data->arg, data->env);
+// }
+
+void	lastcmd(t_data *data)
+{
+	int fd;
+
+	data->file = findpath(data->path, data->av[data->index]);
+	data->cmd = ft_strjoin(data->path[data->file], data->av[data->index]);
+	data->arg = ft_split(data->av[data->index], ' ');
+	fd = open(data->av[data->ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	close(data->fd[data->index - 3][1]);
+	dup2(data->fd[data->index - 3][0], STDIN_FILENO);
+	close(data->fd[data->index - 3][0]);
+	dup2(fd, STDOUT_FILENO);
+	// dprintf(2, "test");
+	// dprintf(2, "arg = %s\n", data->arg[0]);
+	// dprintf(2, "arg = %s\n", data->arg[1]);
+	// dprintf(2, "data->cmd = %s\n", data->cmd);
+	write(2, data->arg[0], ft_strlen(data->arg[0]));
+	execve(data->cmd, data->arg, data->env);	
+}
+
+void	exec(t_data *data)
+{
+	get_cmd(data);
+	// printf("index = %d\n", data->index);
+	// printf("nombre commande %d\n", data->ac - 3);
+	if (data->index == 2)
+	{
+		// printf("firstcmd\n");
+		firstcmd(data);
+	}
+	// else if (data->index <= data->ac - 3)
+	// {
+	// 	printf("midlecmd\n");
+	// 	midlecmd(data);
+	// }
+	else
+	{
+		// printf("lastcmd\n");
+		lastcmd(data);
+	}
+}
+
+
 int	loopfork(t_data *data)
 {
-	int	file;
-	// int	i;
+	int	status;
 
-	// i = 0;
-	file = findpath(data->path, data->av[1]);
-	printf("%s\n", data->path[file]);
-	// if (pipe(data->fd) < 0)
-	// 	return (0);
-	// while (i < ac - 1)
-	// {
-
-	// 	data->pid = fork();
-	// 	if (data->pid == 0)
-	// 	{
-	// 		execve()
-	// 	}
-	// }
+	while (data->index <= data->ac - 2)
+	{
+		if (pipe(data->fd[data->index - 2]) < 0)
+		// if (pipe(data->fd) < 0)
+			return (0);
+		data->pid = fork();
+		if (data->pid < 0)
+			return (0);
+		if (data->pid == 0)
+		{
+			// printf("fork ok\n");
+			exec(data);
+		}
+		else
+		{
+			// close(data->fd[1]);
+			// dup2(data->fd[0], STDIN_FILENO);
+			// close(data->fd[0]);
+			close(data->fd[data->index - 2][1]);
+			dup2(data->fd[data->index - 2][0], STDIN_FILENO);
+			// if (data->index != 2)
+			close(data->fd[data->index - 2][0]);
+			waitpid(data->pid, &status, 0);
+			data->index++;
+		}
+	}
 	return (0);
 }
 
@@ -140,6 +258,6 @@ int	main(int ac, char **av, char **env)
 	// 	i++;
 	// }
 
-	//test1
+	//test1n
 
 }
